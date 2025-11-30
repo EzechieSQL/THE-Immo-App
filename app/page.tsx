0ca02d88236882c407b4262d2592984ec6878956
EzechieSQL <thibaut.tailou@hotmail.fr>
2025-11-30 17:01:41 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontSize: 20 }}>
+      <h1>Bienvenue dans THE-Immo-App</h1>
+      <p>Le front-end va maintenant pouvoir utiliser Supabase.</p>
+    </main>
+  );
+}
